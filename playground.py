eb0ceb5b71f272476a80320fe3347ed4affae70c
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-18 15:12:44 +0530

    done

---
+++ playground.py
@@ -0,0 +1,20 @@
+sentence = 'The quick brown fox jumps over the lazy dog'
+
+# string.split() by default splits the string at spaces
+# original string remains as such
+# result of split is a list of substrings
+# split character will be discarded
+
+words = sentence.split()
+
+print(f'Words: {words}')
+print(f'Type of words: {type(words)}')
+
+underscore_separated_tokens = 'ABC_123456_MX415_%_4587+/_09$#@'
+
+tokens = underscore_separated_tokens.split('_')
+print(f'Tokens: {tokens}')
+
+long_string = 'this is a really long sentence here'
+limited_words = long_string.split(maxsplit=2)
+print(f'Only 2 words: {limited_words}')
