ce973f66594334a58def12659001d03a7a5c9670
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-18 14:34:42 +0530

    done

---
+++ playground.py
@@ -0,0 +1,16 @@
+original_sentence = 'this is a line here'
+
+lower_case_sentence = original_sentence.lower()
+upper_case_sentence = original_sentence.upper()
+
+# First letter of every word is capitalized
+title_case_sentence = original_sentence.title()
+
+# only first letter is capitalized
+capitalized_sentence = original_sentence.capitalize()
+
+print(f'Original: {original_sentence}')
+print(f'Lower case: {lower_case_sentence}')
+print(f'Upper case: {upper_case_sentence}')
+print(f'Title case: {title_case_sentence}')
+print(f'Capitalized: {capitalized_sentence}')
