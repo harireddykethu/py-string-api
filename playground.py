7324c613776ad17997b2c9dc1a60e018200f3ca7
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-18 14:27:54 +0530

    done

---
+++ playground.py
@@ -0,0 +1,10 @@
+# unicode in simple terms here: https://techterms.com/definition/unicode
+# unicode official site here: https://unicode.org/standard/WhatIsUnicode.html
+
+
+greeting = 'Hej där!'
+
+statement = 'नई दिल्ली भारत की राजधानी है'
+
+print(f'Greeting in Swedish: {greeting}')
+print(f'Statement in Hindi: {statement}')
