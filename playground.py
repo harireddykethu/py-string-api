c72ed99c7876dab59b33fee7016e13803a2501d1
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-18 15:33:55 +0530

    done

--- playground.py
+++ playground.py
@@ -1,10 +1,15 @@
 sentence = 'The name that can be named is not the eternal name'
 
-# Syntax: https://docs.python.org/3.8/library/stdtypes.html#str.replace
+# Syntax: https://docs.python.org/3.8/library/stdtypes.html#str.find
 
-new_sentence = sentence.replace('name', 'NAME')
+first_index = sentence.find('name')
 
-new_sentence_with_limit = sentence.replace('name', 'NAME', 1)
+# index of n in the first occurrance of name in the sentence
+print(f'First index of name: {first_index}')
+# index is -1 for no match
+print(f'First index of no match: {sentence.find("tao")}')
 
-print(f'After complete replacement: {new_sentence}')
-print(f'After 1 replacement: {new_sentence_with_limit}')
+# index within bounds. -1 for not finding
+print(f'Index within bounds: {sentence.find("the", 0, 30)}')
+
+# Additional API (from right side): https://docs.python.org/3.8/library/stdtypes.html#str.rfind
